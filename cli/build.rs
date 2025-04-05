d9d98542faa50ef3b7abfd6dbaccd6a01d2aecdc
CodeDoctorDE <codedoctor@linwood.dev>
2025-04-05 22:09:02 +0200

    Install pnpm if cli

--- cli/build.rs
+++ cli/build.rs
@@ -3,22 +3,17 @@ use std::fs;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let project_root = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
-    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    let project_root = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?); // Use CARGO_MANIFEST_DIR
 
-    // Define the source path
+    println!("cargo:warning=Project Root: {}", project_root.display());
+
     let source_path = project_root.join("node_modules/@linwooddev/style/css/main.css");
+    println!("cargo:warning=Source Path: {}", source_path.display());
 
-    // Define the destination path in the output directory
-    let destination_path = out_dir.join("main.css");
+    let destination_path = project_root.join("assets/public/main.css");
 
-    // Copy the file to the output directory
+    fs::create_dir_all(destination_path.parent().unwrap())?;
     fs::copy(&source_path, &destination_path)?;
-
-    // We still want to copy to the public directory for rust_embed
-    let public_destination_path = project_root.join("assets/public/main.css");
-    fs::create_dir_all(public_destination_path.parent().unwrap())?;
-    fs::copy(&source_path, &public_destination_path)?;
 
     println!("cargo:rerun-if-changed={}", source_path.display());
     println!("cargo:rerun-if-changed=public");
